f5c4e00b28da27f483b266e162bcf5a4f819e7ea
pythtesting <166146501+pythtesting@users.noreply.github.com>
2025-02-13 16:03:18 +0100

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 #Pull the docker image from docker hub
-docker pull nityatesting/flask-app:latest
+docker pull nityatesting/flask-app
 
 #Run the Docker image as a container
-docker run -p 5000:5000 nityatesting/flask-app:latest
+docker run -p 5000:5000 nityatesting/flask-app
