d159ba11f43b686c24a864f72534a75954613553
pythtesting <166146501+pythtesting@users.noreply.github.com>
2025-02-13 17:07:40 +0100

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -4,5 +4,5 @@ set -e
 #Stop the running container
 container_id = docker ps | awk -F " " '{print $1}'
 
-#docker rm -f $container_id
+docker rm -f $container_id
 echo "container ID has been deleted"
